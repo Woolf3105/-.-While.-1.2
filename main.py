9d2a5e7a10affac1f637f9fb4f963015b78020ae
Woolf3105 <ElectraBris@gmail.com>
2024-12-05 19:10:51 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-while 1 > 0:
-     my_list = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
-     index = 0
-     while index < len(my_list):
+my_list = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
+index = 0
+while index < len(my_list):
          num = my_list[index]
          if num < 0:
              break
